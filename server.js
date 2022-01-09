ec144c7bac54433143fe8a0d7cb7d4b9a53b4498
e.moghaddami <elhamm.moghaddami@gmail.com>
2022-01-10 00:47:18 +0330

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -6,11 +6,11 @@
     const app = express();
 
     // Serve only the static files form the dist directory
-    app.use(express.static(__dirname + '/dist/study-tube'));
+    app.use(express.static(__dirname + '/dist/user-panel'));
 
     app.get('/*', function(req,res) {
         
-    res.sendFile(path.join(__dirname+'/dist/study-tube/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/user-panel/index.html'));
     });
 
     // Start the app by listening on the default Heroku port
