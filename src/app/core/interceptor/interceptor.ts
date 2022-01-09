db217ce8dfffda8ccb582171df8c962d9d1db1fb
e.moghaddami <elhamm.moghaddami@gmail.com>
2022-01-09 22:37:45 +0330

    Add error handling logic

---
+++ src/app/core/interceptor/interceptor.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpErrorResponse,
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { SnackBarService } from '../services/snack-bar.service';
+
+@Injectable()
+export class NoopInterceptor implements HttpInterceptor {
+  constructor(private snackBService: SnackBarService) {}
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+   
+
+    return next.handle(req).pipe(
+      tap(
+        () => {},
+        (err: any) => {
+          if (err instanceof HttpErrorResponse) {
+            debugger
+            for (const key in err.error.errors) {
+              this.snackBService.openSnackBar(
+                err.error.errors[key],
+                'toastUnSuccess'
+              );
+            }
+           
+          }
+        }
+      )
+    );
+  }
+}
