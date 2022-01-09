fdc7bb0bb175816c79073575676e193ef8797212
e.moghaddami <elhamm.moghaddami@gmail.com>
2022-01-09 21:40:18 +0330

    Add learning routes

---
+++ src/app/learnings/learnings-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LearningsComponent } from './learnings.component';
+
+const routes: Routes = [
+  { path: '', component: LearningsComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class LearningsRoutingModule { }
